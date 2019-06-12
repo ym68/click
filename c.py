b78964116af4c5d46e342a0684948d6808ab6c56
ym68 <ym68@msn.cn>
2019-06-12 09:35:43 +0800

    time synchonization and auto click

---
+++ c.py
@@ -0,0 +1,45 @@
+﻿# -*- coding: utf-8 -*-
+
+
+'''
+
+mouse auto click
+mzg
+2019.1.20
+
+'''
+
+
+import win32api, win32con, win32gui  
+
+from datetime import datetime, date, time, timedelta
+import time
+
+
+def click():
+    x,y=win32gui.GetCursorPos()
+
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+strnext_time = input('请输入开始点击时间，注意输入法切换为英文，如2019-01-19 23:28:00。:') 	
+period = int(input('请输入再下一次自动点击时间间隔，如60 秒。 单位为秒：'))
+print('下一次开始点击时间为 ',strnext_time,'再下一次自动点击时间间隔为 ',period,'秒')
+
+if __name__ == '__main__':
+    while True:
+        iter_now = datetime.now()
+        iter_now_time = iter_now.strftime('%Y-%m-%d %H:%M:%S')
+        if str(iter_now_time) == str(strnext_time):  
+            i=1
+            while i<2:
+                i+=1
+                click()
+                time.sleep(0.2)  #每秒点击5次， 点击1次
+            iter_time = iter_now + timedelta(seconds=period)
+            strnext_time = iter_time.strftime('%Y-%m-%d %H:%M:%S')
+            print(strnext_time)
+            continue
+
+            
---
+++ t.py
@@ -0,0 +1,73 @@
+﻿# -*- coding: utf-8 -*-
+
+
+
+import requests
+import datetime
+import time
+import os
+import ntplib
+from time import ctime
+
+
+# host = 'http://crm.schtrust.com/activeDesktop2.jsp'
+
+# host = 'https://www.baidu.com'
+# host = 'https://www.12306.cn'
+host = 'https://www.taobao.com'
+
+
+
+def get_webservertime(host):
+    headers ={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'} 
+    r = requests.get(host, headers = headers)
+
+    tss=  r.headers['Date'] #获取http头date部分 string
+    # print(ts)
+              
+    # 提取GMT时间的字符串
+    tsp = time.strptime(tss[5:25], "%d %b %Y %H:%M:%S")  # string to time tuple
+    ts = time.mktime(tsp) 
+    tsn = ts + 8*60*60 + 0.86  # timestamp + Beijing time + time delay , then to time tuple
+    tsnp = time.localtime(tsn) #timestamp to local time tuple
+    # print (tsnp)
+
+    dat="Date %u-%02u-%02u"%(tsnp.tm_year,tsnp.tm_mon,tsnp.tm_mday)
+    tm="Time %02u:%02u:%02u"%(tsnp.tm_hour,tsnp.tm_min,tsnp.tm_sec)
+    print ('服务器端',host,'的系统时间是',dat,tm)
+    
+
+    c = ntplib.NTPClient() #创建一个NTPClient的实例
+    s = c.request('ntp5.aliyun.com') #通过对象的request方法得到NTPStats的对象
+    current_time = s.tx_time # time stamp
+    _date, _time = str(datetime.datetime.fromtimestamp(current_time))[:23].split(' ')
+    # print("北京标准时间  ", str(datetime.datetime.fromtimestamp(current_time))[:23])
+
+    dif_time = round((current_time - ts - 8*60*60 ),3)
+    
+    
+    m, s = divmod(dif_time, 60)
+    h, m = divmod(m, 60)
+
+    # print('服务器端时间比北京标准时间慢：', dif_time,'秒')
+    print('服务器端时间比北京标准时间慢：',int(m),'分', s,'秒')
+
+    print ('北京标准时间   ', _date, _time)
+
+    # print(tsnp)
+    
+    print ('本机同步前时间 ', str(datetime.datetime.now())[:23])  #.003, 毫秒级
+
+    os.system(dat)
+    os.system(tm)
+
+ 
+    print ('本机同步后时间 ', str(datetime.datetime.now())[:23])
+
+
+    
+get_webservertime(host) #同步服务器端时间
+
+
+
+
